22dd5c455ca02f17d4336fd73b987627d51aa2a2
Brockify <brdamico@csumb.edu>
2015-07-13 20:19:57 -0700

    Send software / python API to git
    Updated so we have a basic project

---
+++ Python/SkyRealmDatabaseAPI.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+import MySQLdb
+
+db = MySQLdb.connect("173.254.28.39", "skyrealm","AndrewBrock@2013","skyrealm_FindMyPeeps")
+CUR = db.cursor()
+
+#function that asks user for their username and gives them there password
+def get_user_password():
+    username = ""
+    while username != "exit":
+        username = raw_input("What is your username?(Type exit to exit program): ")
+        query = "select all Password from iOweYouUser where Username=%s"
+        response = str(CUR.execute(query, username))
+        if len(response) != None:
+            for x in CUR.fetchall():
+                print("Your password is: " + x[0])
+                break
+            else:
+                print("ERROR: Invalid username.")
+
+get_user_password()
