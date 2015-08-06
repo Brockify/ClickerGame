3bcb261d505c5c7c067133c82b5170d095e0af1f
zenitram56 <bramartinez@csumb.edu>
2015-08-05 19:20:35 -0700

    ----test---Added a Add_friend script and a Friends_list script
    still need to test them in postman before uploading and make tables in
    the database

---
+++ Python/Add_Friend.py
@@ -0,0 +1,15 @@
+#! /usr/bin/python
+print "Context-type: text/html\n\n"
+
+import SkyRealmDatabaseAPI
+import cgi
+cgi.FieldStorage()
+arguments = cgi.FieldStorage()
+UserSending = arguments.getvalue("UserSending")
+UserReceiving = arguments.getvalue("UserReceiving")
+
+
+if UserSending == None or UserReceiving == None:
+    print "parameter missing!"
+else:
+    SkyRealmDatabaseAPI.send_friend_request(UserSending, UserReceiving)
---
+++ Python/Friends_List.py
@@ -0,0 +1,15 @@
+#! /usr/bin/python
+print "Context-type: text/html\n\n"
+
+import SkyRealmDatabaseAPI
+import cgi
+cgi.FieldStorage()
+arguments = cgi.FieldStorage()
+username = arguments.getvalue("username")
+
+
+
+if username == None:
+    print "parameter missing!"
+else:
+    SkyRealmDatabaseAPI.list_friends(username)
